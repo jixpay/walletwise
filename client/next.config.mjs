e7fc81448fc655ad2a54de0d9738bb4415fd9754
irson <jiordesta.dev@gmail.com>
2024-07-21 17:41:38 +0800

    set the .env vars

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['firebasestorage.googleapis.com'],
     },
+    server: {
+        port: process.env.CLIENT_PORT || 3000,
+    },
 };
 
 export default nextConfig;
