1e04027828823d9105348a974af196527ac16c0c
irson <jiordesta.dev@gmail.com>
2024-07-21 20:22:56 +0800

    some changes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: ['https://firebasestorage.googleapis.com/'],
-    },
+        domains: ['firebasestorage.googleapis.com'],
+    }
 };
 
 export default nextConfig;
